3557ac7ca05764a79346d675ced254ad8d9e6b08
ulls12 <koes823@gmail.com>
2023-12-28 12:00:07 +0900

    test

---
+++ Kiosk/Kiosk/Category/CategoryStackView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryStackView.swift
+//  Kiosk
+//
+//  Created by t2023-m0044 on 12/28/23.
+//
+
+import UIKit
+
+class CategoryStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
