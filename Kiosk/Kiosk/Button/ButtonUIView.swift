3c9aefed6898cafb4dc07256c516500bb0d6a039
EojinEva <w-w-w-@naver.com>
2023-12-28 20:34:50 +0900

    튜터님의 손길

--- Kiosk/Kiosk/Button/ButtonUIView.swift
+++ Kiosk/Kiosk/Button/ButtonUIView.swift
@@ -4,17 +4,62 @@
 //
 //  Created by t2023-m0099 on 12/28/23.
 //
+//  스트럭트를 구성하고 델리게이팅을 하면 상속(?)되어 사용할 수 있다?
 
 import UIKit
 
-class ButtonUIView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+//
+protocol ButtonUIViewDelegate: AnyObject { //시키는 곳
+    func present()
 }
+//
+
+
+class ButtonUIView: UIView {
+    
+    //
+    weak var delegate: ButtonUIViewDelegate?
+    //
+    
+    @IBOutlet weak var cancelbutton: UIButton!
+    
+    @IBOutlet weak var paybutton: UIButton!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    func customInit() {
+        if let view = Bundle.main.loadNibNamed("ButtonUIView", owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            addSubview(view)
+            
+        }
+    }
+    
+    
+    
+    
+    @IBAction func cancelbutton(_ sender: Any) {
+        //
+        delegate?.present()
+        //
+        
+        
+        
+        
+        // present뷰컨트롤러의 메서드. 객체간의 소통이다. delegating방식으로? 강의 arc 보기
+    
+    }
+
+    @IBAction func pay(_ sender: Any) {
+    }
+    
+}
+//다른 대상에게 일을 시킬 수 있다 > 델리게이트 패턴으로 구현할 수 있고 대상 타입으로 선언하는 게 아니고 프로토콜 타입으로 선언하여
--- Kiosk/Kiosk/ViewController.swift
+++ Kiosk/Kiosk/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    //Buttonxib연결시도1차
 
     var count: Int = 0  // 장바구니
     
@@ -15,6 +18,10 @@ class ViewController: UIViewController {
     @IBOutlet var totalLabel: UILabel!
     @IBOutlet var total: UILabel!
     @IBOutlet var totalMoney: UILabel!
+    
+    
+    @IBOutlet weak var buttonUIView: ButtonUIView!
+    
     
     // 메인메뉴 햄버거
     @IBAction func mainMenu(_ sender: Any) {
@@ -35,6 +42,7 @@ class ViewController: UIViewController {
         self.totalLabel.text = "\(self.count) 개"
     }
     
+    //최초 호출
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -43,11 +51,26 @@ class ViewController: UIViewController {
         
         total.text = "총주문내역"
         total.textAlignment = .center
-        
+        buttonUIView.delegate = self
+
 
         self.refreshLabel()
     }
     //test
     //home test
 }
-
+extension ViewController: ButtonUIViewDelegate {
+    func present() {
+        
+        let cancelAlert = UIAlertController(title: "결제 취소", message: "결제가 취소됩니다.", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "결제 취소", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        cancelAlert.addAction(okAction)
+        cancelAlert.addAction(cancelAction)
+        
+        self.present(cancelAlert, animated: true)
+    }
+    
+    
+}
