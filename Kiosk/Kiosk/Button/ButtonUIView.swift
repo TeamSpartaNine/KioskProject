6358bb63d11723c26ef55f0998db82bb94dfc056
EojinEva <w-w-w-@naver.com>
2023-12-28 12:05:57 +0900

    되나요

---
+++ Kiosk/Kiosk/Button/ButtonUIView.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonUIView.swift
+//  Kiosk
+//
+//  Created by t2023-m0099 on 12/28/23.
+//
+
+import UIKit
+
+class ButtonUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
