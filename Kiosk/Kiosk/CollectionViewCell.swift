101a7ddd0efa657465fcdfd48f05361f48767d4f
futuremouse <kimsoyoonnv@naver.com>
2023-12-29 22:59:50 +0900

    Merge branch 'main' into image

---
+++ Kiosk/Kiosk/CollectionViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  CollectionViewCell.swift
+//  Kiosk
+//
+//  Created by t2023-m0024 on 12/29/23.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupImageView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupImageView() {
+        addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    func configure(image: UIImage) {
+        imageView.image = image
+    }
+}
+
+
--- Kiosk/Kiosk/ViewController.swift
+++ Kiosk/Kiosk/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    //수정수정수정수정
-
+protocol Zoomable {
+    func zoomIn()
+    func zoomOut()
 }
 
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let zoomButton = ZoomButton()
+        
+        view.addSubview(zoomButton)
+        
+        zoomButton.translatesAutoresizingMaskIntoConstraints = false
+        zoomButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 36).isActive = true
+        zoomButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        zoomButton.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        zoomButton.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        zoomButton.backgroundColor = nil
+        
+    }
+
+}
