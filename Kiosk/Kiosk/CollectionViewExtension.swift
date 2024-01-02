15a1e5769745e6f450711046f0a78057c525a6fa
dahil4 <arhaxoda@naver.com>
2024-01-02 11:56:29 +0900

    extension  분리

---
+++ Kiosk/Kiosk/CollectionViewExtension.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewExtension.swift
+//  NineBurgers
+//
+//  Created by A on 2024/01/02.
+//
+
+import UIKit
+
+class CollectionViewExtension: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Kiosk/Kiosk/TableViewExtension.swift
@@ -0,0 +1,8 @@
+//
+//  TableViewExtension.swift
+//  NineBurgers
+//
+//  Created by A on 2024/01/02.
+//
+
+import Foundation
